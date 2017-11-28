829db7b3438d93329cee758b710bb775ceae997d
romainberthome <romainberthome@gmail.com>
2017-11-28 16:51:15 +0000

    google application

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express"),
+    passport = require("passport"),
+    GoogleStrategy = require("passport-google-oauth20").Strategy,
     app     = express();
-    
+
+
+passport.use(new GoogleStrategy());
+
 app.get('/', function(req, res){
     res.send('Hi there');
 });
